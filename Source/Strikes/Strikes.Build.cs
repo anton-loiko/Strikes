3ba8556d7356544bcca12eefd2c36ca7632760fd
Anton Loiko <anton.loiko.engineer@gmail.com>
2024-08-10 06:38:58 -0500

    feat(PART-1): Implement first part of the tutorial
    - Added core functionality for the first part of the tutorial.
    - Included basic setup and initial logic.
    - Updated relevant files and added necessary comments.
    
    Details:
    - Initialized game state and character properties.
    - Set up basic timers and health/magic management.

--- Source/Strikes/Strikes.Build.cs
+++ Source/Strikes/Strikes.Build.cs
@@ -8,6 +8,10 @@ public class Strikes : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore"
+		});
 	}
 }
+ 
--- Source/Strikes/StrikesCharacter.h
+++ Source/Strikes/StrikesCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
+#include "Components/TimelineComponent.h"
 #include "StrikesCharacter.generated.h"
 
 class UInputComponent;
@@ -36,18 +37,252 @@ class AStrikesCharacter : public ACharacter
 	/** Move Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
 	UInputAction* MoveAction;
-	
+
 public:
 	AStrikesCharacter();
 
 protected:
 	virtual void BeginPlay();
 
+	virtual void Tick(float DeltaTime) override;
+
 public:
-		
 	/** Look Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputAction* LookAction;
+
+	// Health Properties
+
+	/**
+	 * Total amount of health the character can have.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health")
+	float FullHealth;
+
+	/**
+	 * Current health of the character.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health")
+	float Health;
+
+	/**
+	 * Percentage of current health compared to the full health.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health")
+	float HealthPercentage;
+
+	/**
+	 * Previous value of health before the last update.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
+	float PreviousHealth;
+
+	/**
+	 * Indicates if the screen should flash red (e.g., when taking damage).
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health")
+	bool bRedFlash;
+
+	// Magic Properties
+
+	/**
+	 * Total amount of magic the character can have.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Magic")
+	float FullMagic;
+
+	/**
+	 * Current amount of magic the character has.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Magic")
+	float Magic;
+
+	/**
+	 * Percentage of current magic compared to the full magic.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Magic")
+	float MagicPercentage;
+
+	/**
+	 * Previous value of magic before the last update.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Magic")
+	float PreviousMagic;
+
+	/**
+	 * Amount of magic to be used or changed.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Magic")
+	float MagicValue;
+
+	/**
+	 * Curve used to modify magic values over time.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Magic")
+	UCurveFloat* MagicCurve;
+
+
+	/**
+	 * Timeline for managing magic-related events or animations.
+	 */
+	UPROPERTY(EditAnywhere, Category="Magic")
+	FTimeline MyTimeline;
+
+	/**
+	 * Timer handle for managing time-based events related to magic.
+	 */
+	UPROPERTY(EditAnywhere, Category="Magic")
+	FTimerHandle MemberTimerHandler;
+
+	/**
+	 * Timer handle for managing magic-specific time-based events.
+	 */
+	UPROPERTY(EditAnywhere, Category="Magic")
+	FTimerHandle MagicTimerHandler;
+
+	// Internal Properties
+
+	// Value of the curve float for current magic status.
+	float CurveFloatValue;
+
+	// Current value of the timeline for magic events.	
+	float TimeLineValue;
+
+	// Indicates if the character can be damaged.
+	bool bCanBeDamaged;
+
+	// Indicates if the character can use magic.
+	bool bCanUseMagic;
+
+
+	// Health Functions
+
+	/**
+	 * Gets the current health of the character.
+	 * 
+	 * @return Current health value.
+	 */
+	UFUNCTION(BlueprintPure, Category="Health")
+	float GetHealth() const;
+
+	/**
+	 * Gets the current health as a formatted text.
+	 * 
+	 * @return Formatted health text.
+	 */
+	UFUNCTION(BlueprintPure, Category="Health")
+	FText GetHealthIntText() const;
+
+	// Magic Functions
+	/**
+	 * Gets the current amount of magic the character has.
+	 * 
+	 * @return Current magic value.
+	 */
+	UFUNCTION(BlueprintPure, Category="Magic")
+	float GetMagic() const;
+
+	/**
+	 * Gets the current magic amount as a formatted text.
+	 * 
+	 * @return Formatted magic text.
+	 */
+	UFUNCTION(BlueprintPure, Category="Magic")
+	FText GetMagicIntText() const;
+
+	/**
+	 * Function called on a timer to handle damage-related events.
+	 */
+	UFUNCTION()
+	void DamageTimer();
+
+	/**
+	 * Sets the state of the character in response to damage.
+	 */
+	UFUNCTION()
+	void SetDamageState();
+
+	/**
+	 * Sets the amount of magic the character can use.
+	 */
+	UFUNCTION()
+	void SetMagicValue();
+
+	/**
+	 * Sets the state of the magic, including usage or changes.
+	 */
+	UFUNCTION()
+	void SetMagicState();
+
+	/**
+	 * Changes the magic value based on the given input.
+	 * 
+	 * @param MagicChange The new value to set for magic.
+	 */
+	UFUNCTION()
+	void SetMagicChange(float MagicChange);
+
+	/**
+	 * Updates the magic value based on current conditions.
+	 */
+	UFUNCTION()
+	void UpdateMagic();
+
+	/**
+	 * Determines if a red flash effect should be played.
+	 * 
+	 * @return True if the flash should be played, otherwise false.
+	 */
+	UFUNCTION(BlueprintPure, Category="Health")
+	bool PlayFlash();
+
+	/**
+	 * Default material for the weapon.
+	 */
+	UPROPERTY(EditAnywhere, Category="Magic")
+	UMaterialInterface* GunDefaultMaterial;
+
+	/**
+	 * Material applied to the weapon when it overheats.
+	 */
+	UPROPERTY(EditAnywhere, Category="Magic")
+	UMaterialInterface* GunOverheatMaterial;
+
+	/**
+	 * Handles the application of point damage to the character.
+	 * 
+	 * @param Damage The amount of damage to apply.
+	 * @param DamageType Type of damage being applied.
+	 * @param HitLocation Location where the damage hit.
+	 * @param HitNormal Normal vector of the hit surface.
+	 * @param HitComponent Component that was hit.
+	 * @param BoneName Name of the bone that was hit.
+	 * @param ShotFromDirection Direction from which the shot was fired.
+	 * @param InstigatedBy The controller that caused the damage.
+	 * @param DamageCauser The actor that caused the damage.
+	 * @param HitInfo Information about the hit.
+	 */
+	// UFUNCTION()
+	void ReceivePointDamage(float Damage, const class UDamageType* DamageType, FVector HitLocation,
+	                        FVector HitNormal, class UPrimitiveComponent* HitComponent, FName BoneName,
+	                        FVector ShotFromDirection, class AController* InstigatedBy,
+	                        AActor* DamageCauser, const FHitResult& HitInfo
+
+	);
+
+	/**
+	 * Updates the health of the character based on the given change.
+	 * 
+	 * @param HealthChange The amount by which to change the health.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Health")
+	void UpdateHealth(float HealthChange);
+
+	/**
+	 * Resets and initializes the magic-related timers and updates.
+	 * Stops any ongoing magic timeline, clears and sets up a new magic timer, and updates the magic value.
+	 */
+	void InitializeMagicTimers();
 
 protected:
 	/** Called for movement input */
@@ -66,6 +301,4 @@ public:
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
-
 };
-
